fdf0247700cf91008220e63eb2cf6c08f3f89254
sampath kumar <sampath.kumar@housing.com>
2015-08-26 15:06:55 +0530

    finish layout and routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,7 +4,7 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | ROR"
+    assert_select "title", "ROR"
   end
 
   test "should get help" do
@@ -17,5 +17,10 @@ class StaticPagesControllerTest < ActionController::TestCase
     get :about
     assert_response :success
     assert_select "title", "About | ROR"
+  end
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | ROR"
   end
 end
